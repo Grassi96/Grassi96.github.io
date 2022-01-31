4b72708f2772647b449e56361075a97e260ffaa2
Christian <Christian@192.168.0.178>
2022-01-31 17:17:28 +0100

    Test

--- MoneyConverter.java
+++ MoneyConverter.java
@@ -2,8 +2,16 @@ package tutorial;
 
 import java.util.Scanner;
 
+/**
+ *
+ * @author Christian
+ */
 public class MoneyConverter {
     
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bitte gebe einen Betrag in EUR an: ");
@@ -15,17 +23,35 @@ public class MoneyConverter {
         convertToPfund(money);
         convertToSchilling(money);
     }    
+
+    /**
+     *
+     * @param eur
+     */
     public static void convertToDollars(Double eur) {
         System.out.println("Der Betrag in Dollar ist: " + eur * 1.12);
     }
     
+    /**
+     *
+     * @param eur
+     */
     public static void convertToKuna(Double eur) {
          System.out.println("Der Betrag in Kuna ist: " + eur * 7.52);
     }
     
+    /**
+     *
+     * @param eur
+     */
     public static void convertToPfund (Double eur) {
         System.out.println("Der Betrag in Pfund ist: " + eur * 0.84);
     }
+
+    /**
+     *
+     * @param eur
+     */
     public static void convertToSchilling (Double eur) {
         System.out.println("Der Betrag in Schilling ist: " + eur * 13.76);
     }
